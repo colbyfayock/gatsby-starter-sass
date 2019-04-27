c2e7490e7e977ff2f14a3021dbda6858b52f3b57
Colby Fayock <fayock@gmail.com>
2019-04-27 18:00:39 -0400

    adding gatsby start command to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,21 @@ const IndexPage = () => {
           <img src={img_gatsby} alt="Build with Gatsby!" />
         </p>
         <h1>Gatsby Sass Starter</h1>
-        <p>Welcome to your new Gatsby site.</p>
-        <p>Now go build something great.</p>
+        <p>
+          Welcome to your new Gatsby site.
+        </p>
+        <p>
+          Now go build something great.
+        </p>
+        <h2>Still Getting Started?</h2>
+        <p>
+          Run the following in your terminal!
+        </p>
+        <pre>
+          <code>
+            gatsby new [directory] https://github.com/colbyfayock/gatsby-starter-sass
+          </code>
+        </pre>
       </Container>
     </Layout>
   );
